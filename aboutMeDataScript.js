167bac8a5518d133b3c8eee8b943227a87c4e131
Keyshawn Jeannot <143298940+The1keyy@users.noreply.github.com>
2024-12-29 05:36:25 -0500

    making the loop that itrate for each over each meber in that array

--- aboutMeDataScript.js
+++ aboutMeDataScript.js
@@ -3,5 +3,15 @@ const teamContainer = document.getElementById("teamContainer");
 
 // fetch the aboutmedata .jason file
 fetch("aboutMeData.json")
+
     //convert the response so json can understand it
     .then(response => response.json())
+
+    //now data is the array of the team members from aboutmedat.json//
+    .then(data => {
+        // making the loop that cover each member
+        data.array.forEach(element => {
+            const card = document.createElement("div");
+
+        });
+    }
