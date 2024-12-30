cb2b137e55921ff7a9093b7e536625e4652e2d2b
Keyshawn Jeannot <143298940+The1keyy@users.noreply.github.com>
2024-12-30 13:19:04 -0500

    addes system to fill hyml with dynamic binding

--- aboutMeDataScript.js
+++ aboutMeDataScript.js
@@ -10,24 +10,41 @@ fetch("aboutMeData.json")
     //now data is the array of the team members from aboutmedat.json//
     .then(data => {
         // making the loop that cover each member
-        data.array.forEach(element => {
+        data.forEach(element => {
             const card = document.createElement("div");
             card.classList.add("team-card")
 
             // creating an img for the member's photo
             const img = document.createElement("img");
-            img.src = member.photo;
+            img.src = element.photo;
             card.appendChild(img);
 
             // creating a header for the member's name
-            const nameElm = document.createElement("h1");
-            nameElm.textContent = member.name;
-            card.appendChild(nameElem);
+            const nameElm = document.createElement("h3");
+            nameElm.textContent = element.name;
+            card.appendChild(nameElm);
 
             // creating  member's role section
-
+            const roleElm = document.createElement("p")
+            roleElm.textContent = `Role: ${element.role}`;
+            card.appendChild(roleElm);
+            
             //creating a about text
+            const aboutElm = document.createElement("p");
+            aboutElm.textContent = element.about;
+            card.appendChild(aboutElm);
 
             //create a link to their GitHub
+            //const githublink=document.createElement("a");
+            //githublink.href = element.github;
+            //githublink.textContent = "Github";
+            //card.appendChild(githublink)
+
+            //append the card tot the team container
+            teamContainer.appendChild(card)
         });
     })
+     // If something goes wrong, log the error
+  .catch(error => {
+    console.error("Error fetching the JSON data:", error);
+  });
