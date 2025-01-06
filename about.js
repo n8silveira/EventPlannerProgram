1870b02d34a92c54820807445a25821e932ed5d1
n8silveira <nathaniel.silveira10@gmail.com>
2025-01-06 13:56:41 -0500

    fixed up aboutMe files

--- about.js
+++ about.js
@@ -1,13 +1,13 @@
-// get the div in "aboutme page" that the team card will be in//
+// get the div in "aboutMe.json page" that the team card will be in
 const teamContainer = document.getElementById("teamContainer");
 
-// fetch the aboutmedata .jason file
-fetch("aboutMeData.json")
+// fetch aboutMe.json file
+fetch("aboutMe.json")
 
     //convert the response so json can understand it
     .then(response => response.json())
 
-    //now data is the array of the team members from aboutmedat.json//
+    //now data is the array of the team members from aboutMe.json//
     .then(data => {
         // making the loop that cover each member
         data.forEach(element => {
